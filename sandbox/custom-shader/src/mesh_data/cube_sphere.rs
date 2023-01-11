cc33b7d69001f1894b3fa7f08b4fde8afda06026
saltyfishie-fedora <teugene888@gmail.com>
2023-01-11 13:51:47 +0800

    rename create_face_vertices span

--- sandbox/custom-shader/src/mesh_data/cube_sphere.rs
+++ sandbox/custom-shader/src/mesh_data/cube_sphere.rs
@@ -221,7 +221,7 @@ fn create_face_vertices(
     face_direction: Vec3,
     vertex_template: VertexTemplate,
 ) -> VertexData {
-    let _ = info_span!("create_face_vertex", name = "create_face_vertex").entered();
+    let _ = info_span!("create_face_vertices", name = "create_face_vertices").entered();
 
     fn convert_to_sphere_position(pos: Vec3) -> Vec3 {
         let Vec3 { x, y, z } = pos;
